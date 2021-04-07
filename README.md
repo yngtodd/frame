582c41b6c5765c87a978055fe160275325ab9e1f
yngtodd <young.todd.mk@gmail.com>
2021-04-07 12:53:50 -0400

    Initialize repository

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-<h1>Starter<img src='https://github.com/yngtodd/starter/blob/main/img/snek.png' align='right' width='180' height='104'></h1>
+<h1>Frame<img src='https://github.com/yngtodd/frame/blob/main/img/snek.png' align='right' width='180' height='104'></h1>
 
-A starter template for Python packages.
+Seq2Seq modeling for frame semantics.
 
 #### Setup
 
@@ -10,7 +10,7 @@ python setup.py install
 
 #### Features
 
-Starter comes with a built in command line interface:
+Frame comes with a built in command line interface:
 
 ```python
 python -m starter.cli greet
--- setup.py
+++ setup.py
@@ -11,20 +11,19 @@ requirements = ['argh',]
 
 
 COMMANDS = [
-    'greet = starter.cli:greet',
+    'greet = frame.cli:greet',
 ]
 
 
 setup(
-    author="",
-    author_email="",
-    python_requires='>=3.6',
+    author="Todd Young",
+    author_email="yngtdd@gmail.com",
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
@@ -36,11 +35,11 @@ setup(
     include_package_data=True,
     keywords='starter',
     name='starter',
-    packages=find_packages(include=['starter', 'starter.*']),
+    packages=find_packages(include=['frame', 'frame.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/yngtodd/starter',
+    url='https://github.com/yngtodd/frame',
     version='0.1.0',
     zip_safe=False,
 )
