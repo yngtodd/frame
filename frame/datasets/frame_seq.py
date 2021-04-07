5f93cb1fa6406cc57e09222ed77b10dce4fab572
yngtodd <young.todd.mk@gmail.com>
2021-04-07 17:54:29 -0400

    Add docstring

--- frame/datasets/frame_seq.py
+++ frame/datasets/frame_seq.py
@@ -3,9 +3,24 @@ from nltk.corpus.reader import framenet
 
 
 class FrameSeq2Seq(Dataset):
-    """Map sentences to frame definitions"""
+    """Map sentences to frame definitions
 
-    def __init__(self, path):
+    For each sentence, return a dictionary that
+    contains the sentence text, the associated 
+    frame definition, and the frame name.
+
+    Note:
+        Framenet's frame definitions can contain 
+        a summary definition and examples. A robust
+        pipeline should be able to extract these 
+        examples, leaving only the necessary definition
+        bits.
+
+    Args:
+        path: path to the `fndata-1.7` dataset
+    """
+
+    def __init__(self, path: str):
         super().__init__()
         self.path = path
         self.sentences = self._load_framenet()
