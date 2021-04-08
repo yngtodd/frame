18fe79c015df7a27d246867860f986d980146582
yngtodd <young.todd.mk@gmail.com>
2021-04-08 15:12:00 -0400

    Add Trainer

--- examples/frame_seq2seq.py
+++ examples/frame_seq2seq.py
@@ -1,18 +1,29 @@
 """Seq2Seq Summarization - Sentences to Frame Definitions"""
+import nltk
+import numpy as np
+
 from datasets import load_dataset
 from argparse import ArgumentParser
 from frame.framenet import data_paths
-from transformers import AutoTokenizer
+
+from transformers import (
+    AutoTokenizer AutoModelForSeq2SeqLM, 
+    DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+)
 
 
 def parse_args():
     parser = ArgumentParser(description="Seq2Seq Example")
     parser.add_argument("--data", type=str, 
                         help="Root path to processed data")
+    parser.add_argument("--model", type=str, default="t5-small",
+                        help="Root path to processed data")
     parser.add_argument("--max_input_length", type=int, default=1024,
                         help="max sentence length")
     parser.add_argument("--max_target_length", type=int, default=128,
                         help="max frame definition length")
+    parser.add_argument("--batch_size", type=int, default=16,
+                        help="batch size for training and eval")
     return parser.parse_args()
 
 
@@ -23,6 +34,14 @@ def main():
     # using `frame.cli:preprocess-framenet
     data_paths = data_paths(args.data)
     dataset = load_dataset('json', data_files=data_paths)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+
+    # the family of t5 models expect input sentences to be prefixed with `"summarize: "`
+    if args.model in ["t5-small", "t5-base", "t5-larg", "t5-3b", "t5-11b"]:
+        prefix = "summarize: "
+    else:
+        prefix = ""
 
     # HuggingFace loves to use closures, I would prefer this 
     # be refactored into the library, but going this route for simplicity
@@ -36,7 +55,7 @@ def main():
         Args:
             examples: samples in the dataset
         """
-        inputs = [sent for sent in examples["sentence"]]
+        inputs = [prefix + sent for sent in examples["sentence"]]
 
         model_inputs = tokenizer(
             inputs, 
@@ -54,3 +73,56 @@ def main():
         model_inputs["labels"] = labels["input_ids"]
 
         return model_inputs
+
+    # again, with the closures - requires instance of the tokenizer
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        # Replace -100 in the labels as we can't decode them.
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+        # Rouge expects a newline after each sentence
+        decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
+        decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
+
+        result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+        # Extract a few results
+        result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+
+        # Add mean generated length
+        prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
+        result["gen_len"] = np.mean(prediction_lens)
+
+        return {k: round(v, 4) for k, v in result.items()}
+
+    tokenized_dataset = dataset.map(preprocess_function, batched=True)
+    model = AutoModelForSeq2SeqLM.from_pretrained(args.model)
+    data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+
+    training_args = Seq2SeqTrainingArguments(
+        "test-summarization",
+        evaluation_strategy = "epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=args.batch_size,
+        weight_decay=0.01,
+        save_total_limit=3,
+        num_train_epochs=1,
+        predict_with_generate=True,
+        fp16=True,
+    )
+
+    trainer = Seq2SeqTrainer(
+        model,
+        training_args,
+        train_dataset=tokenized_datasets["train"],
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics
+    )
+
+    trainer.train()
+
+
+if __name__=="__main__":
+    main()
