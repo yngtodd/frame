bb2d2bcf00ba8259f011b8062b0403afa58f9ddc
yngtodd <young.todd.mk@gmail.com>
2021-04-08 00:26:00 -0400

    Simplify basline pipeline

--- frame/datasets/frame_seq.py
+++ frame/datasets/frame_seq.py
@@ -25,20 +25,22 @@ class FrameSeq2Seq(Dataset):
         self.path = path
         self.sentences = self._load_framenet()
 
-    def _load_framenet(self, path):
+    def _load_framenet(self):
         """Load framenet sentences generator"""
         fn = framenet.FramenetCorpusReader(self.path, fileids=None)
         return fn.sents()
 
     def __repr__(self):
-        return f"FrameSeq2Seq(path={self.path})"
+        return f"FrameSeq2Seq(path='{self.path}')"
 
     def __len__(self):
         return 100_000
 
     def __getitem__(self, idx):
+        sent = self.sentences[idx]
+
         return {
-            "sentence": self.sentences[idx].text,
-            "frame_definition": self.sentences[idx].frame.definition,
-            "frame": self.sentences[idx].frame.name
+            "sentence": sent.text,
+            "frame_definition": sent.frame.definition,
+            "frame": sent.frame.name
         }
