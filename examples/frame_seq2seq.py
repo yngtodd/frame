005153cef60d01f0736ad96ced7bd5b72cda4fb5
Todd Young (ygx) <ygx@lucky.ornl.gov>
2021-04-08 17:48:36 -0400

    Add train, valid, test splits

--- examples/frame_seq2seq.py
+++ examples/frame_seq2seq.py
@@ -2,9 +2,9 @@
 import nltk
 import numpy as np
 
-from datasets import load_dataset
 from argparse import ArgumentParser
 from frame.framenet import data_paths
+from datasets import load_dataset, DatasetDict
 
 from transformers import (
     AutoTokenizer, AutoModelForSeq2SeqLM, 
@@ -34,6 +34,15 @@ def main():
     # using `frame.cli:preprocess-framenet
     paths = data_paths(args.data)
     dataset = load_dataset('json', data_files=paths)
+
+    train_test = dataset["train"].train_test_split(test_size=0.1)
+    test_valid = train_test["test"].train_test_split(test_size=0.5)
+
+    datasets = DatasetDict({
+        "train": train_test["train"],
+        "test": test_valid["test"],
+        "valid": test_valid["train"]
+    })
 
     tokenizer = AutoTokenizer.from_pretrained(args.model)
 
@@ -96,14 +105,17 @@ def main():
 
         return {k: round(v, 4) for k, v in result.items()}
 
-    tokenized_dataset = dataset.map(preprocess_function, batched=True)
+    tokenized_datasets = datasets.map(preprocess_function, batched=True)
+
     model = AutoModelForSeq2SeqLM.from_pretrained(args.model)
     data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
 
     training_args = Seq2SeqTrainingArguments(
         "./results/summarization",
+        evaluation_strategy = "epoch",
         learning_rate=2e-5,
         per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
         weight_decay=0.01,
         save_total_limit=3,
         num_train_epochs=1,
@@ -114,7 +126,8 @@ def main():
     trainer = Seq2SeqTrainer(
         model,
         training_args,
-        train_dataset=tokenized_dataset["train"],
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["valid"],
         data_collator=data_collator,
         tokenizer=tokenizer,
         compute_metrics=compute_metrics
