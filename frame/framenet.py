8b0f4814c711004e0ca33c4293cba596adfad2b8
yngtodd <young.todd.mk@gmail.com>
2021-04-08 12:05:45 -0400

    Add function for saving json data

--- frame/framenet.py
+++ frame/framenet.py
@@ -85,3 +85,43 @@ def load_sentence_data(save_root):
 
     return sentences
 
+
+def save_json(sentence, filename):
+    """Save sentence frame pairs as json"""
+    data = {
+        "frame": sentence.frame.name, 
+        "sentence": sentence.text, 
+        "frame_definition": sentence.frame.definition
+    }
+
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+
+def save_sentence_json(framenet_path, save_root, num_samples=100_000):
+    """Save sentence data as individual files as json
+
+    Every sample has two documents, a sentence from 
+    Framenet as well as the frame definition. This 
+    can be used for text summarization Seq2Seq models.
+    
+    Note:
+        As a proof of concept, this only saves the first
+        `num_samples`.
+
+    Args:
+        framenet_path: path to the `fndata-1.7` dataset
+        save_root: root path to save individual sentence csvs
+        num_samples: number of samples to save
+    """
+    sentences = load_sentences(framenet_path)
+
+    root = Path(save_root)
+    root.mkdir(parents=True, exist_ok=True)
+
+    for idx in tqdm(range(num_samples)):
+        sent = sentences[idx]
+        # Name the file based on frame type
+        path = root.joinpath(f"{sent.frame.name}_{idx}.json")
+        save_json(sent, path)
+
